2022f73568b7289db45371a4eed1aa41796c0e95
Giovanne Afonso <giovanneafonso@gmail.com>
2023-01-19 21:16:17 -0300

    geospatial analysis

--- src/utils/usgeoutils.py
+++ src/utils/usgeoutils.py
@@ -1,9 +1,14 @@
 import json
-from urllib.request import urlopen
+import os
+import numpy as np
+import pandas as pd
+
 
 class USGEO:
-  def load(self):
-    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-      self.json = json.load(response)
+    def load(self):
+        csv_location = f"{os.environ['PROJECT_ROOT']}/io/customdata/cfips_coordinates.csv"
+        self.cfips_coordinates = pd.read_csv(csv_location).sort_values(by=[
+            'cfips']).reset_index(drop=True)
+
 
 usgeo = USGEO()
